98e7c4738f04995e5e313ecb7b25f271d4f2bc82
XIFI <137690070+XIFIYT@users.noreply.github.com>
2024-04-16 20:19:04 +0200

    Update scaleform.hpp

--- src/Utility/scaleform.hpp
+++ src/Utility/scaleform.hpp
@@ -3,18 +3,5 @@
 
 namespace Big
 {
-	class Scaleform
-	{
-	private:
-		int m_offset;
-		bool m_prepared;
-		int m_scaleForm;
-	public:
-		void PrepareInstructionalButtons();
-		void DrawInstructional(const char* text, const char* button);
-		void DrawLargeMessage(const char* title, const char* message, int duration);
-		void FinishInstructionalButtons();
-	};
-
-	Scaleform* GetScaleform();
+	
 }
